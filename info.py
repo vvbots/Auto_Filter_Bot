59b8375c5dcd974ec7f590293e6f06107af28871
vvbots <vvbots7@gmail.com>
2025-11-16 10:10:33 +0530

    Change default values for API_ID and API_HASH
    Updated API_ID and API_HASH to default to empty strings.

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
 # Bot Information Configuration
 # ============================
 SESSION = environ.get('SESSION', 'dreamxbotz_search')   # Session name for the bot
-API_ID = int(environ.get('API_ID', '27852736')) # API ID from my.telegram.org
-API_HASH = environ.get('API_HASH', '28f69bdb8515b51bffcf114c5e2e3b1b')  # API Hash from my.telegram.org
+API_ID = int(environ.get('API_ID', '')) # API ID from my.telegram.org
+API_HASH = environ.get('API_HASH', '')  # API Hash from my.telegram.org
 BOT_TOKEN = environ.get('BOT_TOKEN', "8297292617:AAEn4SBAKE6T4qLhQELwObvqOFaRl_k5CJQ")    # Bot token from @BotFather
 
 # ============================
